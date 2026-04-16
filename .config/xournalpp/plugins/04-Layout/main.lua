0637406dfa2b5df3f4fbba202d6510f8e6741e7b
codiy <mail@codiy.net>
2026-04-15 20:39:51 -0400

    Update xournalpp layerout plugin

--- .config/xournalpp/plugins/04-Layout/main.lua
+++ .config/xournalpp/plugins/04-Layout/main.lua
@@ -1,3 +1,6 @@
+local lastPressTime = 0
+local lastPressColumn = 0
+
 function initUi()
 	for i = 1, 6 do
 		local funcName = "layout" .. i .. "Column"
@@ -15,12 +18,33 @@ function initUi()
 end
 
 function setLayout(columns)
+	local currentTime = os.clock()
+	local isDoubleTap = false
+
+	if (currentTime - lastPressTime) <= 0.5 and lastPressColumn == columns then
+		isDoubleTap = true
+	end
+
+	if isDoubleTap then
+		lastPressTime = 0
+		lastPressColumn = 0
+	else
+		lastPressTime = currentTime
+		lastPressColumn = columns
+	end
+
 	app.changeActionState("set-columns-or-rows", columns)
 
-	local zoomLevels = { 1, 0.83, 0.55, 0.42, 0.32, 0.30 }
-	local zoomLevel = zoomLevels[columns] or 1
+	local zoomLevelsSingle = { 1, 0.83, 0.55, 0.42, 0.32, 0.30 }
+	local zoomLevelsDouble = { 1, 0.73, 0.48, 0.37, 0.30, 0.30 }
+
+	local zoomLevel = 1
+	if isDoubleTap then
+		zoomLevel = zoomLevelsDouble[columns] or 1
+	else
+		zoomLevel = zoomLevelsSingle[columns] or 1
+	end
 
 	app.setZoom(zoomLevel)
-
 	app.changeActionState("select-tool", app.C.Tool_hand)
 end
